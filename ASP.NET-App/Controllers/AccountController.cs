cb18fc5307f6ed7fe337b03455b34e60f559280e
Natanael Augustin <natanael.augustin@gmail.com>
2023-04-28 14:59:54 +0200

    further added to the folder structure and routing possibilites

---
+++ ASP.NET-App/Controllers/AccountController.cs
@@ -0,0 +1,37 @@
+﻿using ASP.NET_App.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET_App.Controllers
+{
+	public class AccountController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+		public IActionResult Register()
+		{
+			return View();
+		}
+		[HttpPost]
+		public IActionResult Register(AccountRegisterViewModel viewModel)
+		{
+			return View();
+		}
+		public IActionResult Login()
+		{
+			return View();
+		}
+		[HttpPost]
+		public IActionResult Login(AccountLoginViewModel viewModel)
+		{
+			return View();
+		}
+		public IActionResult Logout()
+		{
+			return View();
+		}
+
+	}
+}
+
---
+++ ASP.NET-App/Controllers/AdminController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET_App.Controllers
+{
+	public class AdminController : Controller
+	{
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+	}
+}
